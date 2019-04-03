529653545970ccf9e424206210dcc4f15b8e3843
Yoad Shiran <yoad@cooladata.com>
2019-04-03 13:02:37 +0300

    get ip from host - test

--- src/main/java/com/ccreanga/echoserver/ReverseEchoController.java
+++ src/main/java/com/ccreanga/echoserver/ReverseEchoController.java
@@ -9,19 +9,7 @@ import java.net.UnknownHostException;
 @RestController
 public class ReverseEchoController {
 
-    try {
-        InetAddress host;
-        if (args.length == 0) {
-            host = InetAddress.getLocalHost();
-        } else {
-            host = InetAddress.getByName(args[0]);
-        }
-        System.out.println("Host:'" + host.getHostName()
-                + "' has address: " + host.getHostAddress());
-
-    } catch (UnknownHostException e) {
-        e.printStackTrace();
-    }
+    System.out.println(InetAddress.getByName("IP_ADDR").getHostName());
 
     private static final String DEFAULT_TEXT = "Hi - you might want to pass the text parameter :)";
     @RequestMapping(value="/")
