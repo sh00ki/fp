f69e97365842188d0ef1a3df39da4863f24eee45
Yoad Shiran <yoad@cooladata.com>
2019-04-03 13:00:44 +0300

    get ip from host - test

--- src/main/java/com/ccreanga/echoserver/ReverseEchoController.java
+++ src/main/java/com/ccreanga/echoserver/ReverseEchoController.java
@@ -3,9 +3,25 @@ package com.ccreanga.echoserver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class ReverseEchoController {
+
+    try {
+        InetAddress host;
+        if (args.length == 0) {
+            host = InetAddress.getLocalHost();
+        } else {
+            host = InetAddress.getByName(args[0]);
+        }
+        System.out.println("Host:'" + host.getHostName()
+                + "' has address: " + host.getHostAddress());
+
+    } catch (UnknownHostException e) {
+        e.printStackTrace();
+    }
 
     private static final String DEFAULT_TEXT = "Hi - you might want to pass the text parameter :)";
     @RequestMapping(value="/")
