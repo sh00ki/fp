0d6833c8f9c259e6e1bb4957065b61a5e721ad7f
Yoad Shiran <yoad@cooladata.com>
2019-04-03 13:09:59 +0300

    get ip from host - test

--- src/main/java/com/ccreanga/echoserver/EchoserverApplication.java
+++ src/main/java/com/ccreanga/echoserver/EchoserverApplication.java
@@ -1,4 +1,8 @@
 package com.ccreanga.echoserver;
+package com.crunchify.tutorials;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EchoserverApplication {
 
     public static void main(String[] args) {
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        }
         SpringApplication.run(EchoserverApplication.class, args);
     }
 }
--- src/main/java/com/ccreanga/echoserver/ReverseEchoController.java
+++ src/main/java/com/ccreanga/echoserver/ReverseEchoController.java
@@ -3,26 +3,12 @@ package com.ccreanga.echoserver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @RestController
 public class ReverseEchoController {
-    InetAddress ip;
-    String hostname;
-        try {
-        ip = InetAddress.getLocalHost();
-        hostname = ip.getHostName();
-        System.out.println("Your current IP address : " + ip);
-        System.out.println("Your current Hostname : " + hostname);
 
-    } catch (UnknownHostException e) {
-
-        e.printStackTrace();
-    }
     private static final String DEFAULT_TEXT = "Hi - you might want to pass the text parameter :)";
     @RequestMapping(value="/")
-
     public String reverseEcho(
             @RequestParam(name = "text",required = false,defaultValue = DEFAULT_TEXT) String text) {
 
